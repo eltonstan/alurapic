3e5b534a354a36bff39c4cf0554cda4455a46723
Elton Standerski <eltonstand@gmail.com>
2020-06-12 17:30:09 -0300

    Aula 01 pt2: login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [ SignInComponent ],
     imports: [ 
         CommonModule,
-        ReactiveFormsModule ]
+        ReactiveFormsModule,
+        VMessageModule,
+        RouterModule
+    ]
 })
 export class HomeModule {  }
