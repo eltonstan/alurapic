ca57469336b071d85efa133826274594387eefeb
Elton Standerski <eltonstand@gmail.com>
2020-06-19 12:20:29 -0300

    Adicionando comentarios

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -6,7 +6,6 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
     
-
     constructor(
         private userService: UserService,
         private router: Router) {}
